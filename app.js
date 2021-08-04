173b7513550893caf3f72a1e030fb8e2ce299dd6
paperplane0428 <86367550+paperplane0428@users.noreply.github.com>
2021-08-04 14:29:28 +0900

    index-html-4
    added input field

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+
+/*<h1>Hello 경욱</h1>
+    <p>I'm hosted with GitHub Pages.</p>
+    <ul>
+        <h2>My favorite foods</h2>
+        <li>1. 감자탕</li>
+        <li>2. 삼겹살</li>
+        <li>3. 오므라이스</li>
+        <li>4. 비빔면/쫄면</li>
+        <li>5. 라면</li>
+    </ul> */
+
+/* class Hello extends React.Component {
+  render() {
+    return (
+          <div>
+              <h1>Hello 경욱</h1>
+              <p>I'm hosted with GitHub Pages.</p>
+              <FavFood food={["감자탕", "삼겹살", "오므라이스", "비빔면", "라면"]}/>
+          </div>
+      )
+  }
+}
+class FavFood extends React.Component{
+    render(){
+        
+        const foods = this.props.food.map(food => {
+            return <li>{food}</li>
+        })
+        return (
+            <div>
+              <ul>
+                  <h2>My favorite foods</h2>
+                  {foods}
+              </ul>
+          </div>
+        )
+    }
+} */
